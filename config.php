f2cb98765d605e286f134318f7f4bca275507fef
jaavid <javid.momeni@gmail.com>
2017-07-14 11:01:26 +0430

    Configuration

--- config.php
+++ config.php
@@ -1,15 +1,16 @@
 <?php
-//Robat Token Give from @botfather
-$token 		= "";
-//Group Id To forward Message to group
-$agroup 	= "";
-//Database Configuration
-$dbconf 	= array (
-                'host' 		=> '',
-                'username' 	=> '', 
-                'password' 	=> '',
-                'db' 		=> '',
-                'port' 		=> 3306,
-                'charset' 	=> 'utf8');
+// Add you bot's API key and name
+$bot_api_key  = '';
+$bot_username = '';
+$botan = 'dd110154-77ae-4b43-8e9d-f1700f2f9cae';
+// Define all IDs of admin users in this array (leave as empty array if not used)
+$admin_users = [208627,];
 
+// Enter your MySQL database credentials
+$mysql_credentials = [
+    'host'     => 'localhost',
+    'user'     => '',
+    'password' => '',
+    'database' => '',
+];
 ?>
