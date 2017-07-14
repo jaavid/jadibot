0c33f26c48d35f19fc43ba9611a7a7acabae9965
jaavid <javid.momeni@gmail.com>
2017-07-14 12:30:19 +0430

    Better Comments

--- jcommands/HelpCommand.php
+++ jcommands/HelpCommand.php
@@ -1,19 +1,8 @@
 <?php
-/**
- * This file is part of the TelegramBot package.
- *
- * (c) Avtandil Kikabidze aka LONGMAN <akalongman@gmail.com>
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
-
 namespace Longman\TelegramBot\Commands\UserCommands;
-
 use Longman\TelegramBot\Commands\Command;
 use Longman\TelegramBot\Commands\UserCommand;
 use Longman\TelegramBot\Request;
-
 /**
  * User "/help" command
  */
--- jcommands/StartCommand.php
+++ jcommands/StartCommand.php
@@ -1,15 +1,5 @@
 <?php
-/**
- * This file is part of the TelegramBot package.
- *
- * (c) Avtandil Kikabidze aka LONGMAN <akalongman@gmail.com>
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
-
 namespace Longman\TelegramBot\Commands\SystemCommands;
-
 use Longman\TelegramBot\Commands\SystemCommand;
 use Longman\TelegramBot\Request;
 
